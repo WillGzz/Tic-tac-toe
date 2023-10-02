39a42d3f051dd5fbe3e79c51a96a52626aefb5b1
Will <willygomezr1@gmail.com>
2023-10-02 02:21:15 -0400

    3rd commit

--- Cheryls-tic-tac-toe/main.js
+++ Cheryls-tic-tac-toe/main.js
@@ -3,10 +3,21 @@ console.log(
 );
 
 /*----- constants -----*/
-
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 /*----- app's state (variables) -----*/
 let board;
 let turn = "X";
+let win;
+
 
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll("#board div"));
@@ -14,7 +25,7 @@ const squares = Array.from(document.querySelectorAll("#board div"));
 const messages = document.querySelector('h2');
 /*----- event listeners -----*/
 document.getElementById("board").addEventListener("click", handleTurn); //“grab” an HTML element using getElementById() and then chain the event listener onto it:
-
+document.getElementById('reset-button').addEventListener('click', init);
 /*----- functions -----*/
 function init() {
   board = ["", "", "", "", "", "", "", "", ""];
@@ -30,7 +41,7 @@ function render() {
     //this sets the text content of the square of the same position to the mark on the board.
     squares[index].textContent = mark;
   });
-  messages.textContent = `It's ${turn}'s turn!`;
+  messages.textContent = win === 'T' ? `That's a tie!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
 }
 
 function handleTurn(event) {/*The ‘event’ is the click, the ‘target’ is the element on which the event took place
@@ -38,10 +49,29 @@ function handleTurn(event) {/*The ‘event’ is the click, the ‘target’ is 
 let idx = squares.findIndex(function (square) {
     return square === event.target;
   });
-  board[idx] = turn;
-// check your console logs to make sure it's working!
-turn = turn === 'X' ? 'O' : 'X';
+  if (win) {
+    console.log("Game over. Please start a new game.");
+    return;
+  }
 
-render();
+  if (board[idx] === "") {
+    board[idx] = turn;
+    turn = turn === 'X' ? 'O' : 'X';
+
+win = getWinner();
+  render();
+  }
+ 
 
 }
+function getWinner() {
+    // just stub it up for now.
+    let winner = null;
+winningCombos.forEach(function(combo, index) {
+    if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) 
+    winner = board[combo[0]];
+    
+  });
+ 
+  return winner ? winner : board.includes('') ? null : 'T';
+};
