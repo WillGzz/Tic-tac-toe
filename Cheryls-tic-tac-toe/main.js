736b373a6b1b25770ba20cda9e21e64e03008281
Will <willygomezr1@gmail.com>
2023-10-02 21:37:02 -0400

    made some finishing touches

--- Cheryls-tic-tac-toe/main.js
+++ Cheryls-tic-tac-toe/main.js
@@ -20,8 +20,10 @@ let win;
 
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll("#board div"));
-
+const h2Element = document.querySelector("h2");
 const messages = document.querySelector("h2");
+const grid = Array.from(document.querySelectorAll(".square"));
+const resetButton = document.querySelector("#reset-button");
 /*----- event listeners -----*/
 document.getElementById("board").addEventListener("click", handleTurn); //“grab” an HTML element using getElementById() and then chain the event listener onto it:
 document.getElementById("reset-button").addEventListener("click", init);
@@ -60,6 +62,11 @@ function handleTurn(event) {
 
   if (board[idx] === "") {
     board[idx] = turn;
+    if (turn === "X") {
+      squares[idx].style.color = "red";
+    } else {
+      squares[idx].style.color = "blue";
+    }
     turn = turn === "X" ? "O" : "X";
 
     win = getWinner();
@@ -80,3 +87,37 @@ function getWinner() {
 
   return winner ? winner : board.includes("") ? null : "T";
 }
+let isBlack = true;
+
+// Function to change the color of the h2 element
+function changeColor() {
+  if (isBlack) {
+    h2Element.style.color = "pink";
+  } else {
+    h2Element.style.color = "gold";
+  }
+  // Flip the isBlack variable
+  isBlack = !isBlack;
+}
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+// Function to change the border color of the squares
+function changeBorderColor() {
+  squares.forEach(function (square) {
+    square.style.borderColor = getRandomColor();
+  });
+}
+let colorIndex = 0;
+
+function changeButtonColor() {
+    const colors = ["yellow", "pink"];
+    resetButton.style.color = colors[colorIndex];
+    colorIndex = (colorIndex + 1) % colors.length;
+}
+// Change the color of the h2 element every 1 second
+setInterval(changeColor, 900);
+setInterval(changeBorderColor, 500);
+setInterval(changeButtonColor, 500);
