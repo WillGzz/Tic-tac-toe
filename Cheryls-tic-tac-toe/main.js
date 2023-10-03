15c11a358a1024bb1ce97c078690bcd208d2ebfc
Will <willygomezr1@gmail.com>
2023-10-02 20:16:03 -0400

    Project Done

--- Cheryls-tic-tac-toe/main.js
+++ Cheryls-tic-tac-toe/main.js
@@ -4,32 +4,31 @@ console.log(
 
 /*----- constants -----*/
 const winningCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6], 
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-    ];
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 /*----- app's state (variables) -----*/
 let board;
 let turn = "X";
 let win;
 
-
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll("#board div"));
 
-const messages = document.querySelector('h2');
+const messages = document.querySelector("h2");
 /*----- event listeners -----*/
 document.getElementById("board").addEventListener("click", handleTurn); //“grab” an HTML element using getElementById() and then chain the event listener onto it:
-document.getElementById('reset-button').addEventListener('click', init);
+document.getElementById("reset-button").addEventListener("click", init);
 /*----- functions -----*/
 function init() {
   board = ["", "", "", "", "", "", "", "", ""];
-
+  win = null;
   render();
 }
 
@@ -41,37 +40,43 @@ function render() {
     //this sets the text content of the square of the same position to the mark on the board.
     squares[index].textContent = mark;
   });
-  messages.textContent = win === 'T' ? `That's a tie!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+  messages.textContent =
+    win === "T"
+      ? `That's a tie!`
+      : win
+      ? `${win} wins the game!`
+      : `It's ${turn}'s turn!`;
 }
 
-function handleTurn(event) {/*The ‘event’ is the click, the ‘target’ is the element on which the event took place
+function handleTurn(event) {
+  /*The ‘event’ is the click, the ‘target’ is the element on which the event took place
  — the square we’ve clicked on.  findIndex() finds the index of the square in our squares array that matches the square the user clicked!  */
-let idx = squares.findIndex(function (square) {
+
+  if (win) return;
+
+  let idx = squares.findIndex(function (square) {
     return square === event.target;
   });
-  if (win) {
-    console.log("Game over. Please start a new game.");
-    return;
-  }
 
   if (board[idx] === "") {
     board[idx] = turn;
-    turn = turn === 'X' ? 'O' : 'X';
+    turn = turn === "X" ? "O" : "X";
 
-win = getWinner();
-  render();
+    win = getWinner();
+    render();
   }
- 
-
 }
 function getWinner() {
-    // just stub it up for now.
-    let winner = null;
-winningCombos.forEach(function(combo, index) {
-    if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) 
-    winner = board[combo[0]];
-    
+  // just stub it up for now.
+  let winner = null;
+  winningCombos.forEach(function (combo, index) {
+    if (
+      board[combo[0]] &&
+      board[combo[0]] === board[combo[1]] &&
+      board[combo[0]] === board[combo[2]]
+    )
+      winner = board[combo[0]];
   });
- 
-  return winner ? winner : board.includes('') ? null : 'T';
-};
+
+  return winner ? winner : board.includes("") ? null : "T";
+}
